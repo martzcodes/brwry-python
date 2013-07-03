af6ff94498995fb402767b4776cf9e22a241a5be
Matt Martz <matt.martz@gmail.com>
2013-07-02 22:14:44 -0400

    First Commit
    VERY Basic web framework using Flask

---
+++ README.md
@@ -0,0 +1,7 @@
+# Raspberry Pi Brewery Controller
+
+A lot of this is bound to change.  I will be blogging about this on http://brwry.com/
+
+## Hardware
+
+## Software
--- brwry.py
+++ brwry.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def brwry_main():
-	return render_template('layout.html')
+	return render_template('main_page.html')
+
+@app.route('/about')
+def brwry_about():
+	return render_template('about.html')
+
+@app.route('/configure')
+def brwry_config():
+	return render_template('configure.html')
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8080)
